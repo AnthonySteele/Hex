2d56f9c74e9e40bfdf0deb088ad589b4e37c74b6
AnthonySteele <Anthony@PlasticAvalon.org>
2010-10-26 15:40:07 +0100

    Applied copyright notice

--- Hex.Engine.Test/PathLength/PathLengthTest.cs
+++ Hex.Engine.Test/PathLength/PathLengthTest.cs
@@ -1,3 +1,11 @@
+//-----------------------------------------------------------------------
+// <copyright> 
+// Copyright (c) Anthony Steele 
+//  This source code is part of Hex http://github.com/AnthonySteele/Hex
+//  and is made available under the terms of the Microsoft Reciprocal License (Ms-RL)
+//  http://www.opensource.org/licenses/ms-rl.html
+// </copyright>
+//----------------------------------------------------------------------- 
 namespace Hex.Engine.Test.PathLength
 {
     using System.Collections.Generic;
--- Hex.Engine/Lookahead/DebugDataItem.cs
+++ Hex.Engine/Lookahead/DebugDataItem.cs
@@ -1,4 +1,12 @@
-﻿namespace Hex.Engine.Lookahead
+﻿//-----------------------------------------------------------------------
+// <copyright> 
+// Copyright (c) Anthony Steele 
+//  This source code is part of Hex http://github.com/AnthonySteele/Hex
+//  and is made available under the terms of the Microsoft Reciprocal License (Ms-RL)
+//  http://www.opensource.org/licenses/ms-rl.html
+// </copyright>
+//----------------------------------------------------------------------- 
+namespace Hex.Engine.Lookahead
 {
     using Hex.Board;
 
